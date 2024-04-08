c33adffdff06bb46a072fe1898d1b1218321d395
Cyndi Marren Mustapha <marrencyndi101@gmail.com>
2024-04-08 11:14:22 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const form = document.getElementById('create-task-form');
+
+  form.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    const input = document.getElementById("new-task-description");
+    const taskDescription = input.value;
+
+    const task = document.createElement("li");
+    task.innerHTML = taskDescription;
+
+    const taskList = document.getElementById("tasks");
+    taskList.appendChild(task);
+
+    input.value = "";
+
+  })
 });
